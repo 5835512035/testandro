42014958c243d92de3a79c156bb030d1b02ca94d
5835512035 <5835512035@psu.ac.th>
2018-02-16 12:00:10 +0700

    first commit

---
+++ day2/app/src/main/java/com/god/day2/day2.java
@@ -0,0 +1,24 @@
+package com.god.day2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class day2 extends AppCompatActivity {
+
+        private  TextView god;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day2);
+
+
+
+
+
+    }
+
+
+}
